abd32b380c9d49c62f8bf97004a363fb8d644746
patrickRauer <patrickj.rauer@web.de>
2019-09-24 09:48:50 +0200

    add normalization

--- Phosphorpy/data/sub/spectra.py
+++ Phosphorpy/data/sub/spectra.py
@@ -1,7 +1,12 @@
 from astropy import units as u
+import numpy as np
 
 
 class Spectra:
+    NORMALIZE_MEAN = np.mean
+    NORMALIZE_MEDIAN = np.median
+    NORMALIZE_MAX = np.max
+    NORMALIZE_SUM = np.sum
 
     _wavelength = None
     _flux = None
@@ -17,6 +22,49 @@ class Spectra:
 
         self.wavelength_unit = wavelength_unit
         self.flux_unit = flux_unit
+
+    def normalize(self, kind, inplace=True):
+        """
+        Normalize the spectra with a certain function.
+
+        .. math::
+
+            flux^* = \frac{flux}{function(flux)}
+
+
+        :param kind:
+            The kind of function, which is used to normalize the spectra.
+            Allowed function are the static function or 'mean', 'median', 'max' or 'sum' as ky words.
+        :type kind: function, str
+        :param inplace:
+            True, if the flux should be overwritten with the normalized flux, else False for a new spectra
+            object. Default is True.
+        :type inplace: bool
+        :return: None or if inplace is False, a new spectra object.
+        :rtype: None, Spectra
+        """
+        if type(kind) == str:
+            kind = kind.lower()
+        fl = self.flux.copy()
+        if kind == self.NORMALIZE_MAX or kind == 'max':
+            fl /= np.max(fl)
+        elif kind == self.NORMALIZE_MEAN or kind == 'mean':
+            fl /= np.mean(fl)
+        elif kind == self.NORMALIZE_MEDIAN or kind == 'median':
+            fl /= np.median(fl)
+        elif kind == self.NORMALIZE_SUM or kind == 'sum':
+            fl /= np.sum(fl)
+        else:
+            raise ValueError('Kind of normalization is not allowed.')
+
+        if type(inplace) == bool:
+            if inplace:
+                self._flux = fl
+            else:
+                return Spectra(wavelength=self.wavelength.copy(),
+                               flux=fl)
+        else:
+            raise TypeError('Inplace must be a bool-type.')
 
     @property
     def wavelength(self):
