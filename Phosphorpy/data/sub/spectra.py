50281f33e53f6f6e34156d1e8822f58d8646bb6a
patrickRauer <patrickj.rauer@web.de>
2019-09-24 06:22:04 +0200

    add units to spectra

--- Phosphorpy/data/sub/spectra.py
+++ Phosphorpy/data/sub/spectra.py
@@ -1,9 +1,13 @@
+from astropy import units as u
 
 
 class Spectra:
 
     _wavelength = None
     _flux = None
+
+    _wavelength_unit = None
+    _flux_unit = None
 
     _lines = None
 
@@ -15,5 +19,37 @@ class Spectra:
         return self._wavelength
 
     @property
+    def wavelength_unit(self):
+        return self._wavelength_unit
+
+    @wavelength_unit.setter
+    def wavelength_unit(self, unit):
+        if type(unit) != u.Unit:
+            raise ValueError('The new unit must be a astropy unit.')
+        else:
+            try:
+                factor = (self._wavelength_unit.to(unit))
+                self._wavelength *= factor
+                self._wavelength_unit = unit
+            except u.UnitConversionError:
+                raise ValueError('Unit must be equivalent to a length.')
+
+    @property
     def flux(self):
         return self._flux
+
+    @property
+    def flux_unit(self):
+        return self._flux_unit
+
+    @flux_unit.setter
+    def flux_unit(self, unit):
+        if type(unit) != u.Unit:
+            raise ValueError('The new unit must be a astropy unit.')
+        else:
+            try:
+                factor = (self._flux_unit.to(unit))
+                self._flux *= factor
+                self._flux_unit = unit
+            except u.UnitConversionError:
+                raise ValueError('Unit must be equivalent to a flux.')
