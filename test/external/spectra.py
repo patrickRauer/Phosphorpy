816c2bf097697606a535951b21474eebd8d2d65b
patrickRauer <patrickj.rauer@web.de>
2019-10-13 09:53:37 +0200

    add gama tests

--- test/external/spectra.py
+++ test/external/spectra.py
@@ -38,3 +38,25 @@ class TestGetSDSSSpectra(unittest.TestCase):
             sp = spectra.get_sdss_spectra(self.coords, np.arange(len(self.coords)+1))
 
         sp = spectra.get_sdss_spectra(self.coords, list(np.arange(len(self.coords))))
+
+
+class TestGetGAMASpectra(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.coord = SkyCoord(114.084986*u.deg, 25.144718*u.deg)
+        self.coord2 = SkyCoord(211.73487*u.deg, -1.59471*u.deg)
+        self.coords = SkyCoord(
+            np.array([114.084986, 247.083831, 211.73487])*u.deg,
+            np.array([25.144718, 40.933285, -1.59471])*u.deg
+        )
+
+    def test_single_spectra(self):
+        print('single coordinate')
+        sp = spectra.get_gama_spectra(self.coord2)
+        sp = spectra.get_gama_spectra(self.coord)
+        print(sp)
+
+    def test_multiple_coords(self):
+        print('multiple coordinate')
+        sp = spectra.get_gama_spectra(self.coords)
+        print(sp)
