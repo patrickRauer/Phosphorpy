33b73371ea07152ff0d437c268da537203d40f87
patrickRauer <patrickj.rauer@web.de>
2019-10-13 09:59:06 +0200

    add lamost download tests

--- test/external/spectra.py
+++ test/external/spectra.py
@@ -60,3 +60,25 @@ class TestGetGAMASpectra(unittest.TestCase):
         print('multiple coordinate')
         sp = spectra.get_gama_spectra(self.coords)
         print(sp)
+
+
+class TestGetLAMOSTSpectra(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.coord = SkyCoord(114.084986*u.deg, 25.144718*u.deg)
+        self.coord2 = SkyCoord(211.73487*u.deg, -1.59471*u.deg)
+        self.coords = SkyCoord(
+            np.array([114.084986, 247.083831, 211.73487, 78.8234420000])*u.deg,
+            np.array([25.144718, 40.933285, -1.59471, 38.263469000])*u.deg
+        )
+
+    def test_single_spectra(self):
+        print('single coordinate')
+        sp = spectra.get_lamost_spectra(self.coord2)
+        sp = spectra.get_lamost_spectra(self.coord)
+        print(sp)
+
+    def test_multiple_coords(self):
+        print('multiple coordinate')
+        sp = spectra.get_lamost_spectra(self.coords)
+        print(sp)
