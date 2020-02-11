9113c079d8334afbbba1f85e6097157015d48450
patrickRauer <patrickj.rauer@web.de>
2020-02-11 05:38:49 +0100

    add a download of the survey configuration file, if the config file is missing, to the survey reading

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Phosphorpy',
-    version='0.6.13',
+    version='0.6.14',
     python_requires='>=3.6',
     packages=['Phosphorpy', 'Phosphorpy.data',
               'Phosphorpy.data.sub', 'Phosphorpy.data.sub.plots',
