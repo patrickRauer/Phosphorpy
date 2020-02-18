c60582134e9169ed1efb366cf1a529b6fc44c27c
patrickRauer <patrickj.rauer@web.de>
2020-02-18 06:44:10 +0100

    add option to use a mollweide projection in the coordinate plots or not

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Phosphorpy',
-    version='0.6.15',
+    version='0.6.16',
     python_requires='>=3.6',
     packages=['Phosphorpy', 'Phosphorpy.data',
               'Phosphorpy.data.sub', 'Phosphorpy.data.sub.plots',
