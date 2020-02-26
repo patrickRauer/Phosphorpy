fix 'row_id'/index bug in the average function of the light curves

Phosphorpy/data/sub/light_curve.py
@@ -46,7 +46,7 @@ def _average_light_curve(lc, dt_max):
         dec.append(np.sum(l['dec'].values*err_sq)*err_sq_sum)
     s = lc['survey'].values[0]
     return pd.DataFrame({'mag': mags, 'magerr': errs, 'mjd': mjds,
-                         'row_id': len(errs)*[lc['row_id'].values[0]],
+                         'row_id': len(errs)*[lc.index.values[0]],
                          'ra': ra, 'dec': dec, 'survey': np.linspace(s, s, len(errs))})
 
 